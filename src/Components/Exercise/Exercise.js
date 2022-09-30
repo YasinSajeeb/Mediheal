a4519c9ea728367ac32c7968fa5c0e0013c4034d
YasinSajeeb <yeasinalhossen@gmail.com>
2022-09-30 23:13:47 +0600

    sixth commit

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Exercise = () => {
+const Exercise = (props) => {
+  const {name, img, time, seller} = props.exercise;
     return (
         <div>
             <div className="col">
-    <div className="card h-100">
-      <img src="..." className="card-img-top" alt="..."></img>
+              <div className="card h-100">
+      <img src={img} className="card-img-top" alt=""/>
       <div className="card-body">
         <h5 className="card-title">Card title</h5>
         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
