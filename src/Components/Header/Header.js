9eadf9c810478cffa894e8b7e9fea956c1bc3f1a
YasinSajeeb <yeasinalhossen@gmail.com>
2022-09-30 19:27:07 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav class="navbar bg-light">
+  <div class="container">
+    <a className='navbar-brand' href='#'> <i class="fa-solid fa-house-medical text-primary"></i> <strong className='text-primary'>Heal</strong>Medi</a>
+  </div>
+</nav>
+        </div>
+    );
+};
+
+export default Header;
