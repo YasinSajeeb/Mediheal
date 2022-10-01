5a0740a31c585e30f05005dd0674ad2bb1002364
YasinSajeeb <yeasinalhossen@gmail.com>
2022-10-02 00:27:35 +0600

    elev

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Exercise.css'
-const Exercise = (props) => {
-  const {name, description, img, time, seller} = props.exercise;
+const Exercise = ({exercise, handleAddToList}) => {
+  const {name, description, img, time, seller} = exercise;
     return (
         <div>
             <div className="col">
@@ -11,8 +11,10 @@ const Exercise = (props) => {
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
-        <h6>Time Required: {time}s</h6>
-        <button className='bg-primary px-4 py-2'>Add to list</button>
+        <p className="card-text">Seller: <strong>{seller}</strong> </p>
+        
+        <p>Time Required: <strong>{time}s</strong></p>
+        <button onClick={() => handleAddToList(exercise)} className='bg-primary px-4 py-2'>Add to list</button>
       </div>
     </div>
   </div>
--- src/Components/Exercises/Exercises.js
+++ src/Components/Exercises/Exercises.js
@@ -5,28 +5,36 @@ import Accordion from './Accordion/Accordion';
 const Exercises = () => {
 
     const [exercises, setExercises] = useState([]);
+    const [list, setList] = useState([]);
     useEffect(() =>{
         fetch('exercises.json')
         .then(res => res.json())
         .then(data => setExercises(data))
     }, [])
 
+    const handleAddToList = (exercise) => {
+        console.log(exercise);
+        const newList = [...list, exercise]
+        setList(newList);
+    }
     return (
             <div className="row">
-                <div className="col-md-9">
+                <div className="col-md-9 ps-4">
                   <h5 className='mt-5'>Select Your Exercises to maintain your sound health</h5>
-                <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="row row-cols-1 row-cols-md-3 g-3">
                     {
                         exercises.map(exercise => <Exercise
                         key={exercise.id}
                         exercise={exercise}
+                        handleAddToList={handleAddToList}
                         ></Exercise>)
                     }
                 </div>
                     <Accordion></Accordion>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-3 pt-5 ps-4">
                     <h3>Mello</h3>
+                    <p>Selected Exercises: {list.length}</p>
                 </div>
             </div>
         
